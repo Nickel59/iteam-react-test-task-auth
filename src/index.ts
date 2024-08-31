fd0469d8bc90116926a6e27ee1e9fc5ecaaef615
Nick Butsky <nbutskyi@gmail.com>
2024-08-31 23:58:57 +0300

    Fix for Render

--- src/index.ts
+++ src/index.ts
@@ -63,6 +63,5 @@ app.post("/login", async (req, res) => {
   res.status(200).send({ name: user.name, desiredJobTitle: user.desiredJobTitle, aboutMe: user.aboutMe });
 });
 
-const port = 8080;
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
