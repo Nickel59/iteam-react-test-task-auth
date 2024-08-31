b56db956b7c5346cbd811533e8156cfc04f3b224
Nick Butsky <nbutskyi@gmail.com>
2024-08-31 23:44:30 +0300

    Add a new command

--- src/index.ts
+++ src/index.ts
@@ -63,4 +63,6 @@ app.post("/login", async (req, res) => {
   res.status(200).send({ name: user.name, desiredJobTitle: user.desiredJobTitle, aboutMe: user.aboutMe });
 });
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+const port = 8080;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
